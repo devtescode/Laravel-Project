9abdd7cd26b2d9c76bdb83ccb01d824593d45982
feranmi99 <olamilekanferanmi99@gmail.com>
2024-08-08 17:08:39 +0100

    add new features

--- app/Http/Controllers/V1/Auth/AuthController.php
+++ app/Http/Controllers/V1/Auth/AuthController.php
@@ -31,11 +31,6 @@ class AuthController extends Controller
         $req = $request->validated();
         $user = User::whereEmail($req['email'])->first();
 
-        // if (!$user || !Hash::check($req['password'], $user->password)) {
-        //     return Utils::validateResp([
-        //         'email' => ['The password or user email is invalid.']
-        //     ]);
-        // }
         if (!$user || !Hash::check($req['password'], $user->password)) {
             return response()->json([
                 'message' => 'The password or user email is invalid.',
@@ -51,15 +46,15 @@ class AuthController extends Controller
         ]);
     }
 
-    public function refreshToken(Request $request)
-    {
-        $user = $request->user();
+    // public function refreshToken(Request $request)
+    // {
+    //     $user = $request->user();
 
-        return Utils::successResp([
-            'token' => $user->createToken('authToken')->plainTextToken,
-            'user' => $user,
-        ]);
-    }
+    //     return Utils::successResp([
+    //         'token' => $user->createToken('authToken')->plainTextToken,
+    //         'user' => $user,
+    //     ]);
+    // }
 
     /**
      * log user out
@@ -68,7 +63,21 @@ class AuthController extends Controller
      */
     public function logout(Request $request)
     {
-        $request->user()->tokens()->delete();
+        $user = $request->user();
+        $request->user()->token()->delete();
+
         return Utils::successResp([], 'Successfully logged out');
+    }
+
+    public function user(Request $request)
+    {
+        // if ($request->user()) {
+        return Utils::successResp([
+            'user' => $request->user(),
+        ]);
+        // }
+
+        // return Utils::errorResp('Unauthorized', 401);
+        // return Utils::errorResp('Unauthorized', 401);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\V1\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
-// Example API route
+
 Route::group(['prefix' => 'v1'], function  () {
     Route::post('register', [AuthController::class, "register"]);
     Route::post('login', [AuthController::class, "login"]);
+    Route::post('logout', [AuthController::class, "logout"]);
+    Route::get('user', [AuthController::class, "user"]);
 });
